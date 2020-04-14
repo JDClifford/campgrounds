301a40c53552f081536153361e627331934cf463
John Clifford <johnclifford00@gmail.com>
2020-04-14 10:53:11 -0400

    continue building out app, add db support

--- app.js
+++ app.js
@@ -1,39 +1,25 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({extended:true}));
-
-var list = [
-    {name: "Salmon Creek",
-    image: "https://pixabay.com/get/52e9d5434b5aa414f1dc8460cf2934771438dbf85254794f74277cd69f4c_340.jpg"
-    },
-    {name: "Biscuit Hill",
-    image: "https://pixabay.com/get/54e5d4414356a814f1dc8460cf2934771438dbf85254794f74277cd69f4c_340.jpg"
-    },
-    {name: "Golden Waterfall",
-    image: "https://pixabay.com/get/57e8d7444251ae14f1dc8460cf2934771438dbf85254794f74277cd69f4c_340.jpg"
-    },
-
-    {name: "Salmon Creek",
-    image: "https://pixabay.com/get/52e9d5434b5aa414f1dc8460cf2934771438dbf85254794f74277cd69f4c_340.jpg"
-    },
-    {name: "Biscuit Hill",
-    image: "https://pixabay.com/get/57e8d7444251ae14f1dc8460cf2934771438dbf85254794f74277cd69f4c_340.jpg"
-    },
-    {name: "Golden Waterfall",
-    image: "https://pixabay.com/get/54e5d4414356a814f1dc8460cf2934771438dbf85254794f74277cd69f4c_340.jpg"
-    }
-]
-
+const express   = require("express"),
+    app         = express(), 
+    bodyParser  = require("body-parser"), 
+    mongoose    = require("mongoose")
+    Campground = require("./models/campground");
+    
 app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost:27017/campground", { useUnifiedTopology: true, useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
     res.render("home");
 });
 
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", {list : list})
+    Campground.find({}, (err, campgrounds) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds : campgrounds});
+        }
+    })
 })
 
 app.get("/campgrounds/new", (req, res) => {
@@ -43,11 +29,34 @@ app.get("/campgrounds/new", (req, res) => {
 app.post("/campgrounds", (req, res) => {
     let name = req.body.name;
     let image = req.body.image;
-    let newObj = {name: name, image: image};
-    list.push(newObj);
-    console.log(newObj);
+    let description = req.body.description;
+    Campground.create(
+        {
+            name: name,
+            image: image,
+            description: description
+        }, 
+        (err, campground) => {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("added");
+            }
+        }
+    )
     res.redirect("/campgrounds");
 })
+
+app.get("/campgrounds/:id", (req, res) => {
+    var pickedId = req.params.id;
+    Campground.findById(pickedId, (err, campground) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground: campground});
+        }
+    }
+)})
 
 app.listen(3000, () => {
     console.log("server has started");
